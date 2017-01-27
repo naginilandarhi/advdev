614f88142fccdba2e4828ef5d9622ce3be77d0aa
Nagini Landarhi <24319@ITCINFOTECH.COM>
2017-01-27 17:20:45 +0530

    same as old code

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
@@ -4,7 +4,7 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 
-@Component(immediate=true,metatype=true,label="Enter:",description = "Simple demo ")
+@Component(immediate=true,metatype=true,label="MyFirstOsgiComponent",description = "Simple demo ")
 @Service(MyFirstOsgiComponent.class)
 @Property(label="tick yes or no:",name="osgi.property", boolValue=false,
 description = "Whether or not to schedule this task concurrently")

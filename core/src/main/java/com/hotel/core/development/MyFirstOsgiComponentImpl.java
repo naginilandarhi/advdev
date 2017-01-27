fd709becfd29511f3c8b3a5a23d9829194192843
Nagini Landarhi <24319@ITCINFOTECH.COM>
2017-01-27 16:46:16 +0530

    change in osgi name

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
@@ -4,8 +4,8 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 
-@Component(immediate=true,metatype=true,label="FirstOsgiComponent",description = "Simple demo ")
-@Service(value=MyFirstOsgiComponent.class)
+@Component(immediate=true,metatype=true,label="OsgiComponent",description = "Simple demo ")
+@Service(MyFirstOsgiComponent.class)
 @Property(label="tick yes or no:",name="osgi.property", boolValue=false,
 description = "Whether or not to schedule this task concurrently")
 public class MyFirstOsgiComponentImpl implements MyFirstOsgiComponent {

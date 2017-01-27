a4fabe8ee84fdc8f9e60fc444b2e997472a9dcdb
Nagini Landarhi <24319@ITCINFOTECH.COM>
2017-01-27 16:22:28 +0530

    latest change in osgi

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
@@ -4,7 +4,7 @@ import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Service;
 
-@Component(metatype=true,label="MyFirstOsgiComponent",description = "Simple demo for cron-job like task with properties")
+@Component(metatype=true,label="FirstOsgiComponent",description = "Simple demo ")
 @Service(value=MyFirstOsgiComponent.class)
 @Property(label="tick yes or no:",name="osgi.property", boolValue=false,
 description = "Whether or not to schedule this task concurrently")

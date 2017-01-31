ca741bfe99089cbbf828c494d873c20e285fc6b9
Nagini Landarhi <24319@ITCINFOTECH.COM>
2017-01-31 13:58:03 +0530

    added new property

--- core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
+++ core/src/main/java/com/hotel/core/development/MyFirstOsgiComponentImpl.java
@@ -2,14 +2,23 @@ package com.hotel.core.development;
 
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.PropertyOption;
 import org.apache.felix.scr.annotations.Service;
 import org.osgi.service.component.ComponentContext;
 
 @Component(immediate=true,metatype=true,label="MyFirstOsgiComponent",description = "Simple demo ")
 @Service(MyFirstOsgiComponent.class)
+@Properties({
 @Property(label="tick yes or no:",name="osgi.property", boolValue=false,
-description = "Whether or not to schedule this task concurrently")
+description = "Whether or not to schedule this task concurrently"),
+@Property( label = "Author Gender", description = "Describe Author Gender",
+options = { @PropertyOption(name = "Male", value = "1. Male"),
+			@PropertyOption(name = "Female", value = "2. Female") }, 
+			value = "Female")
+})
+
 public class MyFirstOsgiComponentImpl implements MyFirstOsgiComponent {
 
 	
